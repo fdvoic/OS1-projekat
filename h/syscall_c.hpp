b9d51d19b2ab2d7d00e053d59cbf2b79a3dfa358
fdvoic <119110311+fdvoic@users.noreply.github.com>
2024-05-02 06:32:10 -0700

    Add files via upload

---
+++ h/syscall_c.hpp
@@ -0,0 +1,23 @@
+//
+// Created by os on 5/2/24.
+//
+
+#ifndef SYSCALL_C
+#define SYSCALL_C
+
+#include "../lib/hw.h"
+
+//MemoryAlloc
+void* mem_alloc (size_t size);
+int mem_free(void*);
+
+//Thread
+
+
+//Semaphore
+
+
+//Console
+
+
+#endif //SYSCALL_C_HPP
