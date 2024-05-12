2efd8f9ae8c7f702e22c3cccb72879505591cda1
fdvoic <119110311+fdvoic@users.noreply.github.com>
2024-05-12 02:48:14 -0700

    Add files via upload

---
+++ h/error_codes.hpp
@@ -0,0 +1,18 @@
+//
+// Created by os on 5/10/24.
+//
+
+#ifndef PROJECT_BASE_V1_1_ERROR_CODES
+#define PROJECT_BASE_V1_1_ERROR_CODES
+
+#include "../lib/hw.h"
+
+
+constexpr uint64 MEM_ALLOC = 0x01;
+constexpr uint64 MEM_FREE = 0x02;
+
+
+
+
+
+#endif //PROJECT_BASE_V1_1_ERROR_CODES
